a7c17cd5f34932487f397abf61fdcdf82ed28215
Joyliu290 <joyliu290@hotmail.com>
2017-06-23 09:46:01 -0400

    Outputs the revenue for each item in a list format, working to output the name of the product as well.

--- app/src/main/java/joyli/example/com/shopifyinterntrialchallenge/MainActivity.java
+++ app/src/main/java/joyli/example/com/shopifyinterntrialchallenge/MainActivity.java
@@ -1,9 +1,14 @@
 package joyli.example.com.shopifyinterntrialchallenge;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
@@ -13,12 +18,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    SQLdatabaseActivity myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        myDB = new SQLdatabaseActivity(this);
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         Ion.with(this)
                 .load("https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
@@ -32,21 +40,29 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject json = new JSONObject(result);
                             JSONArray orders = json.getJSONArray("orders");
 
-                            for (int i=0;i<orders.length();i++)
-                            {
+                            for (int i = 0; i < orders.length(); i++) {
                                 JSONObject each = orders.getJSONObject(i);
                                 String price = each.getString("total_price");
                                 Log.v("totalPrice " + i, price);
-                                JSONArray individualItem =each.getJSONArray("line_items");
+                                JSONArray individualItem = each.getJSONArray("line_items");
 
-                                for (int j=0;j<individualItem.length();j++)
-                                {
+                                for (int j = 0; j < individualItem.length(); j++) {
                                     JSONObject eachItem = individualItem.getJSONObject(j);
                                     String price2 = eachItem.getString("price");
                                     String nameOfItem = eachItem.getString("name");
                                     String quantity = eachItem.getString("quantity");
+                                    Float revenue = Float.valueOf(price2) * Float.valueOf(quantity);
 
-                                    Log.v("individual name " + j, nameOfItem);
+                                    String newRevenue = revenue.toString();
+                                    if (newRevenue.length() != 0) {
+                                        AddData(newRevenue);
+
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "ERROR!", Toast.LENGTH_LONG).show();
+                                    }
+
+
+                                    Log.v("quantity " + j, quantity);
                                 }
                             }
 
@@ -58,6 +74,29 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        progressBar.setVisibility(View.INVISIBLE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent deadPixel = new Intent(getApplicationContext(),ViewList.class);
+                startActivity(deadPixel);
+            }
+        }, 5000);
+
+    }
+
+    public void AddData(String newEntry1) {
+        boolean insertData = myDB.addData(newEntry1);
+
+        if (insertData == true) {
+
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        } else {
+
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
+
---
+++ app/src/main/java/joyli/example/com/shopifyinterntrialchallenge/SQLdatabaseActivity.java
@@ -0,0 +1,63 @@
+package joyli.example.com.shopifyinterntrialchallenge;
+
+/**
+ * Created by Joyli on 2017-06-22.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLdatabaseActivity extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "mylist.db";
+    public static final String TABLE_NAME = "mylist_data";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "ITEM1";
+
+    public SQLdatabaseActivity(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { //called when the database is created for the first time. This iswehre the creation of tables and the initial population of the tables should happen
+
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + " ITEM1 TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String item1) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        //ContentValues contentValues2 = new ContentValues();
+        contentValues.put(COL_2, item1);
+        //contentValues2.put(COL_2, item2);
+
+        float result = db.insert(TABLE_NAME, null, contentValues);
+
+
+        //if data is inserted incorrectly it will return a -1
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public Cursor getListContents() {
+        SQLiteDatabase db = this.getWritableDatabase(); //create and/ore open a database that will be used for reading and writing
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return data;
+    }
+}
